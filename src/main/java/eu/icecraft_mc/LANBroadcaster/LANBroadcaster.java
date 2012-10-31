6bb25e189d23aac21e3fe7ae256f89bc74450223
Edmond Poon <sagaciouszzzz@gmail.com>
2012-10-31 02:48:32 -0400

    Added ability to broadcast over all network interfaces.

--- src/main/java/eu/icecraft_mc/LANBroadcaster/LANBroadcaster.java
+++ src/main/java/eu/icecraft_mc/LANBroadcaster/LANBroadcaster.java
@@ -2,8 +2,15 @@ package eu.icecraft_mc.LANBroadcaster;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 
@@ -11,40 +18,44 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LANBroadcaster extends JavaPlugin {
-    final static String payload = "[MOTD]%s:%s[/MOTD][AD]%s[/AD]";
-    final static String multicastAddress = "224.0.2.60";
+    final static String MULTICAST_ADDRESS = "224.0.2.60";
+    final static String PAYLOAD = "[MOTD]%s[/MOTD][AD]%s:%s[/AD]";
     final static int PORT = 4445;
 
+    private final List<MulticastSocket> boundSockets = new ArrayList<MulticastSocket>();;
+
     private InetAddress group;
-    private MulticastSocket socket;
 
     @Override
     public void onEnable() {
         try {
-            this.group = InetAddress.getByName(multicastAddress);
-            this.socket = new MulticastSocket(this.PORT);
+            this.group = InetAddress.getByName(LANBroadcaster.MULTICAST_ADDRESS);
 
-            this.socket.setTimeToLive(3);
-            this.socket.joinGroup(this.group);
+            this.bindSockets();
 
-            final String actualIp = Bukkit.getIp().length() == 0 ? InetAddress.getLocalHost().getHostAddress() : Bukkit.getIp();
-            final String formattedPayload = String.format(this.payload, Bukkit.getMotd(), Bukkit.getPort(), actualIp);
-            this.getLogger().info("Multicast packet payload: " + formattedPayload);
-
-            final byte[] b = formattedPayload.getBytes();
-            final DatagramPacket d = new DatagramPacket(b, b.length, this.group, this.PORT);
+            // Create packets for each device
+            final List<DatagramPacket> packets = new ArrayList<DatagramPacket>();
+            for (final MulticastSocket ms : this.boundSockets) {
+                final DatagramPacket datagramPacket = this.createDatagramPacket(ms.getLocalAddress());
+                packets.add(datagramPacket);
+            }
 
             Bukkit.getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
-                AtomicBoolean running = new AtomicBoolean();
+                final AtomicBoolean running = new AtomicBoolean();
 
                 public void run() {
-                    try {
-                        if (running.compareAndSet(false, true)) {
-                            LANBroadcaster.this.socket.send(d);
-                            running.set(false);
+                    if (this.running.compareAndSet(false, true)) {
+                        try {
+                            for (int i = 0; i < LANBroadcaster.this.boundSockets.size(); i++) {
+                                LANBroadcaster.this.boundSockets.get(i).send(packets.get(i));
+                            }
+                            this.running.set(false);
+
+                        } catch (final IOException e) {
+                            throw new RuntimeException(LANBroadcaster.this.toString() + " failed to send broadcast packet", e);
+                        } finally {
+                            this.running.set(false);
                         }
-                    } catch (final IOException e) {
-                        throw new RuntimeException(LANBroadcaster.this.toString() + " failed to send broadcast packet", e);
                     }
                 }
             }, 0, 30l);
@@ -57,6 +68,67 @@ public class LANBroadcaster extends JavaPlugin {
     @Override
     public void onDisable() {
         Bukkit.getScheduler().cancelTasks(this);
-        this.socket.close();
+        for (final MulticastSocket ms : this.boundSockets) {
+            ms.close();
+        }
+    }
+
+    private DatagramPacket createDatagramPacket(InetAddress address) {
+        if (address == null) {
+            throw new IllegalArgumentException();
+        }
+        final String s = String.format(LANBroadcaster.PAYLOAD, Bukkit.getMotd(), address.getHostAddress(), Bukkit.getPort());
+        this.getLogger().info("Multicast packet payload: " + s);
+        final byte[] b = s.getBytes();
+        return new DatagramPacket(b, b.length, this.group, LANBroadcaster.PORT);
+    }
+
+    /**
+     * 
+     * @return List<NetworkInterface> excluding loop back interfaces
+     * @throws SocketException
+     */
+    private static List<NetworkInterface> discoverNetworkInterfaces() throws SocketException {
+        final List<NetworkInterface> list = new ArrayList<NetworkInterface>();
+        final Enumeration<NetworkInterface> nicEnum = NetworkInterface.getNetworkInterfaces();
+        while (nicEnum.hasMoreElements()) {
+            final NetworkInterface nic = nicEnum.nextElement();
+            if (nic.isLoopback()) {
+                continue;
+            }
+            list.add(nic);
+        }
+        return list;
+    }
+
+    private static List<InetAddress> discoverAddresses(NetworkInterface nic, boolean onlyIpv4) {
+        final List<InetAddress> list = new ArrayList<InetAddress>();
+
+        final Enumeration<InetAddress> addressEnum = nic.getInetAddresses();
+        while (addressEnum.hasMoreElements()) {
+            final InetAddress address = addressEnum.nextElement();
+            if (onlyIpv4 && address instanceof Inet6Address) {
+                continue;
+            }
+            list.add(address);
+        }
+        return list;
+    }
+
+    private void bindSockets() throws IOException {
+        // bind sockets to each network device
+        for (final NetworkInterface nic : LANBroadcaster.discoverNetworkInterfaces()) {
+            for (final InetAddress inetAddress : LANBroadcaster.discoverAddresses(nic, true)) {
+                this.getLogger().info("Binding to " + inetAddress.getHostAddress());
+                final MulticastSocket ms = new MulticastSocket(new InetSocketAddress(inetAddress, LANBroadcaster.PORT));
+                ms.setSoTimeout(1);
+                ms.joinGroup(this.group);
+                ms.setBroadcast(true);
+                if (!ms.isBound()) {
+                    throw new IllegalStateException();
+                }
+                this.boundSockets.add(ms);
+            }
+        }
     }
 }
